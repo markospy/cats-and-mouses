f274f9fa8a97d2c5f4803a01809c0dc7856d6f24
Marcos Antonio Avila Morales <mis.redes.work@gmail.com>
2025-02-26 22:15:14 -0500

    `Fixed comparison operators in status printouts for gato and raton`

--- hello.py
+++ hello.py
@@ -49,10 +49,10 @@ def main():
     while gato.vida > 0 and raton.vida > 0:
         print(f"\nTurno {turno._turno + 1}\n")
         print(
-            f"\t🐱 Gato {gato.nombre}:\n\t\t❤️Vida: {gato.vida}\n\t\t💪Fuerza: {gato.fuerza}\n\t\t🛡️Defensa: {gato.defensa}\n\t\t💨Agilidad: {gato.agilidad}\n\t\t🔋Vitalidad: {gato.vitalidad}\n\t\t🐟Comida: {comida_gato.cantidad} - Puede?: {gato.alimentarse==1}\n\t\t🕳️Refugio: {refugio.calidad} - Puede?: {gato.refugiarse==1}"
+            f"\t🐱 Gato {gato.nombre}:\n\t\t❤️Vida: {gato.vida}\n\t\t💪Fuerza: {gato.fuerza}\n\t\t🛡️Defensa: {gato.defensa}\n\t\t💨Agilidad: {gato.agilidad}\n\t\t🔋Vitalidad: {gato.vitalidad}\n\t\t🐟Comida: {comida_gato.cantidad} - Puede?: {gato.alimentarse!=0}\n\t\t🕳️Refugio: {refugio.calidad} - Puede?: {gato.refugiarse!=0}"
         )
         print(
-            f"\t🐭 Raton {raton.nombre}:\n\t\t❤️Vida: {raton.vida}\n\t\t💪Fuerza: {raton.fuerza}\n\t\t🛡️Defensa: {raton.defensa}\n\t\t👁️‍🗨️Inteligencia: {raton.inteligencia}\n\t\t🔋Vitalidad: {raton.vitalidad}\n\t\t🧀Comida: {comida_raton.cantidad} - Puede?: {raton.alimentarse==1}\n\t\t🕳️Refugio: {refugio.calidad} - Puede?: {raton.refugiarse==1}"
+            f"\t🐭 Raton {raton.nombre}:\n\t\t❤️Vida: {raton.vida}\n\t\t💪Fuerza: {raton.fuerza}\n\t\t🛡️Defensa: {raton.defensa}\n\t\t👁️‍🗨️Inteligencia: {raton.inteligencia}\n\t\t🔋Vitalidad: {raton.vitalidad}\n\t\t🧀Comida: {comida_raton.cantidad} - Puede?: {raton.alimentarse!=0}\n\t\t🕳️Refugio: {refugio.calidad} - Puede?: {raton.refugiarse!=0}"
         )
 
         animal = choice(["gato", "raton"])
@@ -61,9 +61,9 @@ def main():
             print(f"\nAcciones para {gato.nombre} (Gato) 🐱:")
             acciones_gato = []
             acciones_disponibles = acciones.copy()
-            if gato.alimentarse == 1:
+            if gato.alimentarse != 0:
                 acciones_disponibles.remove("alimentarse")
-            if gato.refugiarse == 1:
+            if gato.refugiarse != 0:
                 acciones_disponibles.remove("esconderse")
             for i in range(CANT_ACCIONES):
                 accion = prompt(
@@ -78,9 +78,9 @@ def main():
             print(f"\nAcciones para {raton.nombre} (Ratón) 🐭:")
             acciones_raton = []
             acciones_disponibles = acciones.copy()
-            if raton.alimentarse == 1:
+            if raton.alimentarse != 0:
                 acciones_disponibles.remove("alimentarse")
-            if raton.refugiarse == 1:
+            if raton.refugiarse != 0:
                 acciones_disponibles.remove("esconderse")
             for i in range(CANT_ACCIONES):
                 accion = prompt(
